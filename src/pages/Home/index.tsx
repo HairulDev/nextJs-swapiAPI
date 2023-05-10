4403231825c247e4eb21d00c4b1c6becd4983a95
Hairul <98758226+HairulDev@users.noreply.github.com>
2023-05-10 14:05:04 +0700

    feat(index): redesign ui

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import styles from '../../styles/Home.module.css';
 import { MyContext } from 'context/MyContextProvider';
 import Loader from '../../components/Loader';
+import { Planet } from '../../context/Interface';
 
 const MainStyled = styled.main`
   align-items: center;
@@ -12,21 +13,8 @@ const MainStyled = styled.main`
   flex-direction: column;
   justify-content: center;
   min-height: 100vh;
-  padding: 4rem 0;
+  padding: 1rem 0;
 `;
-
-interface Planet {
-  name: string;
-  rotation_period: string;
-  orbital_period: string;
-  diameter: string;
-  gravity: string;
-  surface_water: string;
-  population: string;
-  url: string;
-  created: string;
-  edited: string;
-}
 
 const Home: NextPage = () => {
   const {
@@ -43,54 +31,61 @@ const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <MainStyled>
-        {/* <h1 className="text-3xl font-bold underline">SWAPI</h1> */}
-
-        <div className={styles.grid}>
-          {isLoading ? (
-            <Loader />
-          ) : (
-            <table className="table-auto border-collapse border border-slate-500">
-              <thead>
-                <tr>
-                  <th className="px-4 py-2 border border-slate-600">Name</th>
-                  <th className="px-4 py-2 border border-slate-600">Rotation period</th>
-                  <th className="px-4 py-2 border border-slate-600">Orbital period</th>
-                  <th className="px-4 py-2 border border-slate-600">Diameter</th>
-                  <th className="px-4 py-2 border border-slate-600">Gravity</th>
-                  <th className="px-4 py-2 border border-slate-600">Surface water</th>
-                  <th className="px-4 py-2 border border-slate-600">Population</th>
-                  <th className="px-4 py-2 border border-slate-600">Url</th>
-                </tr>
-              </thead>
-              <tbody>
-                {planets.results.map((planet: Planet, index: number) => (
-                  <tr key={index}>
-                    <td className="border px-4 py-2 border border-slate-600">{planet.name}</td>
-                    <td className="border px-4 py-2 border border-slate-600">
-                      {planet.rotation_period}
-                    </td>
-                    <td className="border px-4 py-2 border border-slate-600">
-                      {planet.orbital_period}
-                    </td>
-                    <td className="border px-4 py-2 border border-slate-600">{planet.diameter}</td>
-                    <td className="border px-4 py-2 border border-slate-600">{planet.gravity}</td>
-                    <td className="border px-4 py-2 border border-slate-600">
-                      {planet.surface_water}
-                    </td>
-                    <td className="border px-4 py-2 border border-slate-600">
-                      {planet.population}
-                    </td>
-                    <td
-                      className="border px-4 py-2 border border-slate-600"
-                      onClick={() => handleDetailPlanet(planet.url)}
-                    >
-                      Detail
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          )}
+        <div className="p-5 flex flex-col flex-1 items-center justify-start w-full">
+          <div className={styles.grid}>
+            {isLoading ? (
+              <Loader />
+            ) : (
+              <div className="w-full p-2 my-2 glassmorphism">
+                <table className="table-auto border-collapse border border-slate-500 bg-transparent text-gray-200 text-sm">
+                  <caption className="caption-top mb-2 text-2sm font-bold ">Planets</caption>
+                  <thead>
+                    <tr>
+                      <th className="px-4 py-2 border border-slate-600">Name</th>
+                      <th className="px-4 py-2 border border-slate-600">Rotation period</th>
+                      <th className="px-4 py-2 border border-slate-600">Orbital period</th>
+                      <th className="px-4 py-2 border border-slate-600">Diameter</th>
+                      <th className="px-4 py-2 border border-slate-600">Gravity</th>
+                      <th className="px-4 py-2 border border-slate-600">Surface water</th>
+                      <th className="px-4 py-2 border border-slate-600">Population</th>
+                      <th className="px-4 py-2 border border-slate-600">Detail</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {planets.results.map((planet: Planet, index: number) => (
+                      <tr key={index}>
+                        <td className="border px-4 py-2 border border-slate-600">{planet.name}</td>
+                        <td className="border px-4 py-2 border border-slate-600">
+                          {planet.rotation_period}
+                        </td>
+                        <td className="border px-4 py-2 border border-slate-600">
+                          {planet.orbital_period}
+                        </td>
+                        <td className="border px-4 py-2 border border-slate-600">
+                          {planet.diameter}
+                        </td>
+                        <td className="border px-4 py-2 border border-slate-600">
+                          {planet.gravity}
+                        </td>
+                        <td className="border px-4 py-2 border border-slate-600">
+                          {planet.surface_water}
+                        </td>
+                        <td className="border px-4 py-2 border border-slate-600">
+                          {planet.population}
+                        </td>
+                        <td
+                          className="border px-4 py-2 border border-slate-600"
+                          onClick={() => handleDetailPlanet(planet.url)}
+                        >
+                          Detail
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            )}
+          </div>
         </div>
 
         {isModalOpen && (
@@ -102,65 +97,69 @@ const Home: NextPage = () => {
                 onClick={closeModal}
               ></div>
 
-              <div className="inline-block max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
-                <div className="text-2xl font-bold mb-4">Detail Planet</div>
+              <div
+                className={
+                  'inline-block max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform shadow-xl rounded-2xl glassmorphism'
+                }
+              >
+                <div className="text-2sm font-bold mb-4 text-white">Detail Planet</div>
 
-                <table className="table-auto border-collapse border border-slate-500">
+                <table className="table-auto border-collapse border border-slate-500 text-gray-200 text-sm">
                   <tbody>
                     <tr>
-                      <th className="px-4 py-2 border border-slate-600">Name</th>
+                      <td className="px-4 py-2 border border-slate-600">Name</td>
                       <td className="border px-4 py-2 border border-slate-600">
                         {detailPlanet.name}
                       </td>
                     </tr>
                     <tr>
-                      <th className="px-4 py-2 border border-slate-600">Rotation period</th>
+                      <td className="px-4 py-2 border border-slate-600">Rotation period</td>
                       <td className="border px-4 py-2 border border-slate-600">
                         {detailPlanet.rotation_period}
                       </td>
                     </tr>
                     <tr>
-                      <th className="px-4 py-2 border border-slate-600">Orbital period</th>
+                      <td className="px-4 py-2 border border-slate-600">Orbital period</td>
                       <td className="border px-4 py-2 border border-slate-600">
                         {detailPlanet.orbital_period}
                       </td>
                     </tr>
                     <tr>
-                      <th className="px-4 py-2 border border-slate-600">Diameter</th>
+                      <td className="px-4 py-2 border border-slate-600">Diameter</td>
                       <td className="border px-4 py-2 border border-slate-600">
                         {detailPlanet.diameter}
                       </td>
                     </tr>
                     <tr>
-                      <th className="px-4 py-2 border border-slate-600">Gravity</th>
+                      <td className="px-4 py-2 border border-slate-600">Gravity</td>
                       <td className="border px-4 py-2 border border-slate-600">
                         {detailPlanet.gravity}
                       </td>
                     </tr>
                     <tr>
-                      <th className="px-4 py-2 border border-slate-600">Surface water</th>
+                      <td className="px-4 py-2 border border-slate-600">Surface water</td>
                       <td className="border px-4 py-2 border border-slate-600">
                         {detailPlanet.surface_water}
                       </td>
                     </tr>
                     <tr>
-                      <th className="px-4 py-2 border border-slate-600">Population</th>
+                      <td className="px-4 py-2 border border-slate-600">Population</td>
                       <td className="border px-4 py-2 border border-slate-600">
                         {detailPlanet.population}
                       </td>
                     </tr>
                     <tr>
-                      <th className="px-4 py-2 border border-slate-600">Created</th>
+                      <td className="px-4 py-2 border border-slate-600">Created</td>
                       <td className="border px-4 py-2 border border-slate-600">
                         {convertDateTime(detailPlanet.created)}
                       </td>
                     </tr>
                   </tbody>
                 </table>
-                <div className="mt-6">
+                <div className="mt-5">
                   <button
                     type="button"
-                    className="inline-flex justify-center px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+                    className="inline-flex justify-center px-4 py-2 text-sm font-medium text-white bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
                     onClick={closeModal}
                   >
                     Close
